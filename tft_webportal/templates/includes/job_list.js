1e26e196e155474867cf36e424d7276ad1ee6ac5
sahil19896 <sahil19896@gmail.com>
2025-12-30 15:53:54 +0000

    job list and render fixes

--- tft_webportal/templates/includes/job_list.js
+++ tft_webportal/templates/includes/job_list.js
@@ -1,6 +1,11 @@
 window.jobs = {};
 window.jobs.limit = 20;
 
+/* API call for job data */
+jobs.call = function (args, callback, url="/") {
+	return frappe.call({type: "POST", url: url, args: args, callback: callback, freeze: true,});
+}
+
 frappe.ready(() => {
 	const statusSelect = document.getElementById("filter_status");
 	const pickupInput = document.getElementById("filter_pickup");
@@ -41,4 +46,39 @@ frappe.ready(() => {
 		el.addEventListener("input", applyFilters);
 		el.addEventListener("change", applyFilters);
 	});
+
+	/* ---------------- load more jobs ---------------- */
+	$(".load-more").click(function () {
+		let current_limit = window.jobs.limit + 20;
+		window.jobs.limit = current_limit;
+		console.log(`Fetching designs with limit ${current_limit}`);
+		fetchJobs(current_limit);
+	});
 });
+
+function fetchJobs(limit) {
+	// Fetch jobs from your backend API (replace with your API endpoint)
+    let args = {};
+	args.limit = limit;
+	args.cmd = "tft_webportal.www.job_list.load_my_jobs"
+	jobs.call(args, function (r) {
+		let jobs = r.message || [];
+		let table = $(".job-body");
+		table.empty();
+		if (jobs.length === 0) {
+			table.append('<div class="job-row"><div></div><div></div><div>{{ _("No transportation requests found.") }}</div><div></div><div></div><div></div></div>')
+            return;
+        }
+		jobs.forEach((c, a) => {
+			let row = $(`<div class="job-row" data-status="${a.status.toLowerCase()}" data-pickup="${a.from_address}" data-delivery="${a.to_address}">`);
+			row.append(`<div>${c}</div>`);
+			row.append(`<div>${a.date}</div>`);
+			row.append(`<div>${a.from_address}</div>`);
+			row.append(`<div>${a.to_address}</div>`);
+			row.append(`<div>${a.vehicles}</div>`);
+			row.append(`<div><span class="status-pill status-${ a.status.toLowerCase() }">${ a.status }</span></div>`);
+			row.append(`<div><a class="row-link btn" href="/transportation_request?request=${a.name}&view=1">${ _("View") }</a></div>`);
+			table.append(row);
+        });
+	});
+}
